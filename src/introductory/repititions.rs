ffd3b145de19fdd666a2e9abb44120aef560d59d
RS2007 <rohithsuresh207@gmail.com>
2024-01-08 21:06:08 +0530

    repititions

--- src/introductory/repititions.rs
+++ src/introductory/repititions.rs
@@ -1,3 +1,33 @@
+use std::{io, cmp::max};
+
 pub fn main() {
-    println!("Hello")
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Cannot read from stdin");
+    let char_array = input.chars().collect::<Vec<char>>();
+    let mut l: u64 = 0;
+    let mut r: u64 = 1;
+    match char_array.len() {
+        1 => {
+            println!("1")
+        }
+        2 => {
+            if char_array[0] == char_array[1] {
+                println!("2");
+            }else{
+                println!("1");
+            }
+        }
+        _ => {
+            let mut max_till_now = 1;
+            while l < char_array.len() as u64 && r < char_array.len() as u64 {
+                if char_array[l as usize] == char_array[r as usize] {
+                    r+=1;
+                    max_till_now = max(max_till_now,r-l);
+                } else {
+                    l+=1;
+                }
+            }
+            println!("{max_till_now}");
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 // use introductory::missing_number;
 
-use crate::introductory::weird_algo;
+/* use crate::introductory::weird_algo; */
+
+use introductory::repititions;
 
 mod introductory;
 
 pub fn main() {
-    weird_algo::main();
+    /* weird_algo::main(); */
     // missing_number::main();
+    repititions::main();
 }
